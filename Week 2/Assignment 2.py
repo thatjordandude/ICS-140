3d2de957a39e0f37eb29884591e92eb6334934d8
Jordan <jordanians@yahoo.com>
2023-09-06 08:54:11 -0500

    Added to week 2

---
+++ Week 2/Assignment 2.py
@@ -0,0 +1,7 @@
+length = int(input("Enter the length: "))
+width = int(input("Enter the width: "))
+area = length * width
+perimeter = 2 * (length + width)
+print(f"Your length is: {length} and your width is: {width}")
+print(f"Area of the rectangle: {area}")
+print(f"Perimeter of the rectangle: {perimeter}")
